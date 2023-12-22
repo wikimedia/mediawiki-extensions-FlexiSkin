3013be3b648a720ad8738ed10bd80b8f37ffa37a
hslater <hamish.slater@hallowelt.com>
2023-12-22 19:52:34 +1300

    Use new ResourceLoader namespace
    ERM29303
    
    [REL1_39, REL1_39-2.1.x, master]
    
    Change-Id: I97a010d88d0d65518e3b0d098d6fc05261c657a1
    (cherry picked from commit e3607823779b7a9c43bd99633d7df609835dacda)

--- src/ResourceLoader/Configurator.php
+++ src/ResourceLoader/Configurator.php
@@ -4,9 +4,9 @@ namespace MediaWiki\Extension\FlexiSkin\ResourceLoader;
 
 use MediaWiki\Extension\FlexiSkin\IPlugin;
 use MediaWiki\MediaWikiServices;
+use MediaWiki\ResourceLoader\Context as ResourceLoaderContext;
+use MediaWiki\ResourceLoader\FileModule as ResourceLoaderFileModule;
 use OutputPage;
-use ResourceLoaderContext;
-use ResourceLoaderFileModule;
 use RuntimeException;
 
 class Configurator extends ResourceLoaderFileModule {
