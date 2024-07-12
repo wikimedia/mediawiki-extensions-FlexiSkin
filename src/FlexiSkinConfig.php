3997476830367c913c9627f2d50fd0b0b03519dd
Mschlindwein <schlindwein@hallowelt.com>
2024-07-12 15:57:35 +0200

    Add config to fix logo
    ERM37942
    
    [4.5.x]
    
    Change-Id: Icf1491b25bc345dde980b10a610ccc7234a3a000

---
+++ src/FlexiSkinConfig.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace MediaWiki\Extension\FlexiSkin;
+
+use Config;
+use MediaWiki\MediaWikiServices;
+use RequestContext;
+
+class FlexiSkinConfig implements Config {
+	/** @var array */
+	private $config = [];
+
+	/** @var bool */
+	private $initialized = false;
+
+	/** @var array */
+	private $supportedSettings = [
+		'Logos',
+		'Favicon'
+	];
+
+	private function init() {
+		if ( $this->initialized ) {
+			return;
+		}
+
+		$skinname = $this->getSkinname();
+		$services = MediaWikiServices::getInstance();
+		/** @var IFlexiSkinManager $flexiSkinManager */
+		$flexiSkinManager = $services->get( 'FlexiSkinManager' );
+		$active = $flexiSkinManager->getActive( $skinname );
+		if ( !$active instanceof IFlexiSkin ) {
+			$this->initialized = true;
+			return;
+		}
+
+		$config = $flexiSkinManager->getActiveConfig( $skinname );
+		$logoUrl = $config['images']['logo']['url'] ?? null;
+		if ( $logoUrl ) {
+			$this->config['Logos'] = [
+				'1x' => $logoUrl
+			];
+		}
+
+		$faviconUrl = $config['images']['favicon']['url'] ?? null;
+		if ( $faviconUrl ) {
+			$this->config['Favicon'] = $faviconUrl;
+		}
+
+		$this->initialized = true;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function get( $name ) {
+		$this->init();
+		return $this->config[$name] ?? null;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function has( $name ) {
+		if ( !in_array( $name, $this->supportedSettings ) ) {
+			return false;
+		}
+
+		$this->init();
+		return isset( $this->config[$name] );
+	}
+
+	/**
+	 * @return string
+	 */
+	private function getSkinname() {
+		$context = RequestContext::getMain();
+		if ( defined( 'MW_NO_SESSION' ) && MW_NO_SESSION ) {
+			return $context->getRequest()->getVal( 'skin', '' );
+		}
+		$skinname = $context->getSkin()->getSkinName();
+		return $skinname;
+	}
+}
