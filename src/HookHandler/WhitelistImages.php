0429d4e2ac8b7a1fdb7095e408dcf60dfa129837
d.savuljesku <d.savuljesku@gmail.com>
2026-04-09 09:39:14 +0200

    Fix whitelisting logic of logo and favicon
    [5.1.x]
    
    ERM46324
    
    Depends-On: I3e942c49e9dbf6f94919964e7206897d4b43b788
    
    Change-Id: I6968eae606ada1c9fb278199d95f37fc14c7c446

--- src/HookHandler/WhitelistImages.php
+++ src/HookHandler/WhitelistImages.php
@@ -2,6 +2,8 @@
 
 namespace MediaWiki\Extension\FlexiSkin\HookHandler;
 
+use MediaWiki\Extension\FlexiSkin\IFlexiSkin;
+use MediaWiki\MediaWikiServices;
 use MediaWiki\Title\Title;
 
 class WhitelistImages {
@@ -14,8 +16,16 @@ class WhitelistImages {
 	 * @return bool
 	 */
 	public function onCheckFileWhitelisted( Title $title, string $path, string $name, bool &$whitelisted ): bool {
-		if ( str_starts_with( $name, 'flexiskin-image' ) ) {
-			// $whitelisted = true;
+		$activeSkin = MediaWikiServices::getInstance()->getService( 'FlexiSkinManager' )->getActive();
+		if ( !( $activeSkin instanceof IFlexiSkin ) ) {
+			return true;
+		}
+		$config = $activeSkin->getConfig();
+		$logoFilename = $config['images']['logo']['filename'] ?? null;
+		$faviconFilename = $config['images']['favicon']['filename'] ?? null;
+		if ( ( $logoFilename !== null && $name === $logoFilename ) ||
+			( $faviconFilename !== null && $name === $faviconFilename ) ) {
+			$whitelisted = true;
 			return false;
 		}
 		return true;
